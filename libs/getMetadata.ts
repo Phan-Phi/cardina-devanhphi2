7d1239b3613e0e5afae0218d193156ae9578e7ad
Phan-Phi <phananhphi.pp@gmail.com>
2024-03-26 16:48:34 +0700

    Update: update seo3

--- libs/getMetadata.ts
+++ libs/getMetadata.ts
@@ -35,20 +35,20 @@ const getMetadata = (data: any): Metadata => {
       apple: fvImage,
     },
 
-    // ...(twitterData && {
-    //   twitter: {
-    //     title: twitterData[0].title,
-    //     description: twitterData[0].description,
+    ...(twitterData && {
+      twitter: {
+        title: twitterData[0].title,
+        description: twitterData[0].description,
 
-    //     images: [
-    //       {
-    //         url: `${process.env.NEXT_PUBLIC_BACKEND_API}${twitterData[0].image.data.attributes.url}`,
-    //         alt: twitterData[0].title,
-    //         type: "image/jpeg",
-    //       },
-    //     ],
-    //   },
-    // }),
+        images: [
+          {
+            url: `${process.env.NEXT_PUBLIC_BACKEND_API}${twitterData[0].image.data.attributes.url}`,
+            alt: twitterData[0].title,
+            type: "image/jpeg",
+          },
+        ],
+      },
+    }),
 
     openGraph: {
       title: metaTitle || "",
